aad701a1ab7701aadb81bd43906b20462957085c
Kamil Krawiec <KamilKrawiec1355@interia.pl>
2025-11-15 19:13:13 +0100

    Initial commit

--- elearning_prawo/kursy/admin.py
+++ elearning_prawo/kursy/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Kurs, Uzytkownik_kurs, Rozdzial, Artykul, Pytanie, Odpowiedz
+from .models import Kurs, Uzytkownik_kurs, Rozdzial, Artykul, Pytanie, Odpowiedz,ZapisArtykulu,Notatka,Komentarz,StatystykiPytania,ProgressPytan,WynikiEgzaminu,OcenaArtykulu
 
 admin.site.register(Kurs)
 admin.site.register(Uzytkownik_kurs)
@@ -7,3 +7,10 @@ admin.site.register(Rozdzial)
 admin.site.register(Artykul)
 admin.site.register(Pytanie)
 admin.site.register(Odpowiedz)
+admin.site.register(ZapisArtykulu)
+admin.site.register(Notatka)
+admin.site.register(Komentarz)
+admin.site.register(StatystykiPytania)
+admin.site.register(ProgressPytan)
+admin.site.register(WynikiEgzaminu)
+admin.site.register(OcenaArtykulu)
