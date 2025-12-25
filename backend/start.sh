f68d5e4b5f658ee1f51025f3532f05c32fe27649
Kamil Krawiec <KamilKrawiec1355@interia.pl>
2025-12-25 17:18:56 +0100

    minor fix

--- backend/start.sh
+++ backend/start.sh
@@ -10,7 +10,7 @@ python manage.py createsuperuser --noinput || true
 # KLUCZOWA ZMIANA: Uruchamiamy ciężkie skrypty w tle za pomocą '&'
 echo "==> Uruchamianie skryptów w tle (aby nie blokować startu)..."
 python manage.py scrapuj_dane &
-python manage.py chat_gpt_api &
+python manage.py chat_gtp_api &
 
 echo "==> Uruchamianie serwera Gunicorn..."
 # exec sprawia, że Gunicorn staje się głównym procesem kontenera
